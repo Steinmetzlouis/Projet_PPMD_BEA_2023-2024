fee7a1ef5acc1abe49f8f3d06fafa7a7f37daa57
louise-redlinger <louise.redlinger@ensg.eu>
2024-01-27 16:17:49 +0100

    maj code plugin

--- aero_data_visualizer_dialog.py
+++ aero_data_visualizer_dialog.py
@@ -35,6 +35,8 @@ from qgis.PyQt.QtWidgets import QApplication, QMessageBox
 from qgis.gui import QgsMapTool, QgsMapToolExtent, QgsRubberBand, QgsMapCanvas
 
 from PyQt5.QtCore import Qt
+import json
+from qgis.core import QgsVectorLayer, QgsDataSourceUri, QgsProject, QgsWkbTypes
 
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
@@ -42,72 +44,10 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(
 
 
 
-class MapToolSelectExtent(QgsMapTool):
-    def __init__(self, canvas, callback):
-        super(MapToolSelectExtent, self).__init__(canvas)
-        self.canvas = canvas
-        self.callback = callback
-        self.start_point = None
-
-    def canvasPressEvent(self, event):
-        # Enregistrer le point de départ de la sélection
-        self.start_point = self.toMapCoordinates(event.pos())
-
-    def canvasReleaseEvent(self, event):
-        if self.start_point is None:
-            return
-
-        # Récupérer le point de fin de la sélection
-        end_point = self.toMapCoordinates(event.pos())
-
-        # Créer une emprise rectangulaire à partir des deux points
-        extent = QgsRectangle(self.start_point, end_point)
-
-        # Appeler la fonction callback avec l'emprise sélectionnée
-        if callable(self.callback):
-            self.callback(extent)
-
-        # Réinitialiser le point de départ
-        self.start_point = None
-
-
-class SaisirEmpriseMapTool(QgsMapTool):
-    def __init__(self, canvas):
-        super(SaisirEmpriseMapTool, self).__init__(canvas)
-        self.canvas = canvas
-        self.emprise_defined = False
-        self.emprise = QgsRectangle()
-
-    def canvasPressEvent(self, event):
-        x = event.pos().x()
-        y = event.pos().y()
-        point = self.canvas.getCoordinateTransform().toMapCoordinates(x, y)
-        
-        if not self.emprise_defined:
-            self.emprise.set(QgsPointXY(point), QgsPointXY(point))
-        else:
-            self.emprise_defined = False
-            self.emprise = QgsRectangle()
-
-    def canvasMoveEvent(self, event):
-        if not self.emprise_defined:
-            x = event.pos().x()
-            y = event.pos().y()
-            point = self.canvas.getCoordinateTransform().toMapCoordinates(x, y)
-            self.emprise.set(QgsPointXY(self.emprise.xMinimum(), self.emprise.yMinimum()), QgsPointXY(point))
-
-    def canvasReleaseEvent(self, event):
-        if not self.emprise_defined:
-            self.emprise_defined = True
-
-    def is_emprise_defined(self):
-        return self.emprise_defined
-
-    def get_emprise(self):
-        return self.emprise
 
 
 class AeroDataVisualizerDialog(QtWidgets.QDialog, FORM_CLASS):
+    
     def __init__(self, iface, parent=None):
         """Constructor."""
         super(AeroDataVisualizerDialog, self).__init__(parent)
@@ -117,10 +57,11 @@ class AeroDataVisualizerDialog(QtWidgets.QDialog, FORM_CLASS):
 
         print("Bienvenue dans le bien nommé plugin AeroDataVisualizer ! ")
 
-        # self.mapCanvas.hide()
         self.rubberband = None
         self.mapTool = None
         self.extent = None
+        self.extent_wgs84 = None
+        self.date_value = None
 
         # Pour trouver le QTabWidget 
         tab_widget = self.findChild(QtWidgets.QTabWidget, 'tabWidget')
@@ -148,7 +89,9 @@ class AeroDataVisualizerDialog(QtWidgets.QDialog, FORM_CLASS):
                 coverageButton.clicked.connect(self.on_saisir_emprise_clicked)
 
     def handle_extent_selected(self):
-        extent = self.mapTool.extent()
+        
+        #étendue
+        self.extent = self.mapTool.extent()
 
         # Définir le système de référence source (le CRS actuel de la carte)
         source_crs = self.iface.mapCanvas().mapSettings().destinationCrs()
@@ -160,27 +103,29 @@ class AeroDataVisualizerDialog(QtWidgets.QDialog, FORM_CLASS):
         transform = QgsCoordinateTransform(source_crs, target_crs, QgsProject.instance())
 
         # Appliquer la transformation aux coins de l'emprise
-        extent_wgs84 = transform.transformBoundingBox(extent, QgsCoordinateTransform.ForwardTransform)
+        self.extent_wgs84 = transform.transformBoundingBox(self.extent, QgsCoordinateTransform.ForwardTransform)
         extent_wgs84_str = 'xmin: {:.2f}, ymin: {:.2f}, xmax: {:.2f}, ymax: {:.2f}'.format(
-            round(extent_wgs84.xMinimum(), 2),
-            round(extent_wgs84.yMinimum(), 2),
-            round(extent_wgs84.xMaximum(), 2),
-            round(extent_wgs84.yMaximum(), 2)
+            round(self.extent_wgs84.xMinimum(), 2),
+            round(self.extent_wgs84.yMinimum(), 2),
+            round(self.extent_wgs84.xMaximum(), 2),
+            round(self.extent_wgs84.yMaximum(), 2)
         )
 
+        print(self.extent)
+        print(self.extent_wgs84)
         print("Emprise sélectionnée (WGS84):", extent_wgs84_str)
         self.empriseLabel.setText(f'Emprise définie : {extent_wgs84_str}')
 
         self.mapTool.clearRubberBand()
 
         self.mapCanvas.setLayers(self.iface.mapCanvas().layers())
-        self.mapCanvas.setExtent(extent)
+        self.mapCanvas.setExtent(self.extent)
         self.mapCanvas.setCanvasColor(Qt.white)
         self.mapCanvas.enableAntiAliasing(True)
         if self.rubberband is not None:
             self.mapCanvas.scene().removeItem(self.rubberband)
         self.rubberband = QgsRubberBand(self.mapCanvas, QgsWkbTypes.PolygonGeometry)  # polygon
-        self.rubberband.setToGeometry(QgsGeometry.fromRect(extent), None)
+        self.rubberband.setToGeometry(QgsGeometry.fromRect(self.extent), None)
         self.mapCanvas.refreshAllLayers()
         self.mapCanvas.show()
 
@@ -190,54 +135,43 @@ class AeroDataVisualizerDialog(QtWidgets.QDialog, FORM_CLASS):
         self.activateWindow()
 
     def on_valider_clicked(self):
-        # self.mapTool.deactivate()
+
         self.iface.mapCanvas().unsetMapTool(self.mapTool)
         
-        print('clic valider')
-
         if self.surveyDate:
             # On récupère la valeur de la date saisie
-            date_value = self.surveyDate.date().toString("yyyy-MM-dd")
-            print("Date saisie :", date_value)
+            self.date_value = self.surveyDate.date().toString("yyyy-MM-dd")
+            print(self.date_value)
         else:
             print("Objet QDateEdit non trouvé.")
+
+        if self.extent_wgs84 is not None :
+
+            print("coucou")
+            
+            rect = self.extent_wgs84
+            xmin, ymin, xmax, ymax = rect.xMinimum(), rect.yMinimum(), rect.xMaximum(), rect.yMaximum()
+
+            print("xmin",xmin)
+
+        else :
+
+            print('non')
         
         self.tabWidget.setCurrentIndex(1)
 
     def on_saisir_emprise_clicked(self):
+        
+        #cache la fenetre du plugin
         self.showMinimized()
 
-        # select_extent_tool = MapToolSelectExtent(self.iface.mapCanvas(), self.handle_extent_selected)
-        # self.iface.mapCanvas().setMapTool(select_extent_tool)
+
 
         canvas = self.iface.mapCanvas()
         self.mapTool = QgsMapToolExtent(canvas)
         self.mapTool.extentChanged.connect(self.handle_extent_selected)
         canvas.setMapTool(self.mapTool)
 
-        # print('clic saisir emprise')
-        #
-        # # On crée et on active l'outil de carte personnalisé map_tool
-        # map_tool = SaisirEmpriseMapTool(self.iface.mapCanvas())
-        # self.iface.mapCanvas().setMapTool(map_tool)
-        #
-        # # Tant que l'utilisateur n'a pas saisi l'emprise, l'activité en cours continue
-        # while not map_tool.is_emprise_defined():
-        #     QApplication.processEvents()
-        #
-        # # On récupère l'emprise définie par l'utilisateur
-        # emprise = map_tool.get_emprise()
-        # print(f'Emprise définie: {emprise.toString()}')
-        #
-        # empriseLabel = self.findChild(QtWidgets.QLabel, 'empriseLabel')
-        #
-        # if empriseLabel :
-        #
-        #     print('empriseLabel trouvé')
-        #     empriseLabel.setText(f'Emprise définie : {emprise.toString()}')
-        #
-        # else :
-        #
-        #     print('pas trouvé')
+
     
 
